cc171ab1bf5c65d0f10befcf4a15d59d83ca1a1f
celestebickham <90701404+celestebickham@users.noreply.github.com>
2023-01-18 08:24:40 -0600

    bom3

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -2,32 +2,6 @@ const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
-button.addEventListener('click', () => {
-  const myItem = input.value;
-  input.value = '';
-
-  const listItem = document.createElement('li');
-  const listText = document.createElement('span');
-  const listBtn = document.createElement('button');
-
-  listItem.appendChild(listText);
-  listText.textContent = myItem;
-  listItem.appendChild(listBtn);
-  listBtn.textContent = '\u274C';
-  list.appendChild(listItem);
-
-  listBtn.addEventListener('click', () => {
-    list.removeChild(listItem);
-  });
-
-  input.focus();
-});
-
-// existing variable declarations referencing DOM elements
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('#list');
-
 // add this array declaration and assignment statement
 let chaptersArray = getChapterList() || [];
 chaptersArray.forEach(chapter => {
