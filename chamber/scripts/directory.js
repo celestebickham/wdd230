e1e1235d29c11966d04adcc5d2fe19d0ebee8577
celestebickham <90701404+celestebickham@users.noreply.github.com>
2023-02-02 07:28:20 -0600

    json

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,81 @@
+
+const url = "./directory.json";
+let filterval = "";
+
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+if(sPage == "chamber.html"){
+    filterval = "gold";
+}
+else {
+    filterval = "all";
+}
+
+
+const getDirectory = async (filter = filterval) => {
+	let company = await jsonFetch(url);
+    switch (filter) {
+		case "gold":
+			company = company.filter((seedata) => seedata.level === "Gold");
+			break;
+		default:
+			break;
+	}
+	displayDirectory(company);
+};
+
+async function jsonFetch(url) {
+	const response = await fetch(url);
+	const data = await response.json();
+	return data.member;
+}
+
+
+const displayDirectory = (company) => {
+	const cards = document.querySelector("div.cards");
+	cards.innerHTML = "";
+
+	company.forEach((seedata) => {
+		let card = document.createElement("div");
+        let stats = document.createElement("div");
+		stats.classList.add("stats");
+		let name = document.createElement("p");		
+		let address = document.createElement("p");
+		let phone = document.createElement("p");
+		let url = document.createElement("p");
+		let level = document.createElement("p");
+		let length = document.createElement("p");
+		//let other = document.createElement("p");
+		let image = document.createElement("img");
+
+		name.textContent = `${seedata.name}`;
+		address.innerHTML = ` ${seedata.address}`;
+		phone.innerHTML = ` ${seedata.phone}`;
+		url.innerHTML = `<a href="http://${seedata.url}">${seedata.url}</a>`;
+		level.innerHTML = `${seedata.level} Member`;
+		//other.innerHTML = `<span class="label">Other:</span> ${seedata.other}`;
+
+		image.setAttribute("src", seedata.image);
+		image.setAttribute(
+			"alt",
+			`${seedata.name}`
+		);
+		image.setAttribute("loading", "lazy");
+		image.setAttribute("width", "150");
+
+		stats.appendChild(image);
+		stats.appendChild(name);
+		stats.appendChild(address);
+		stats.appendChild(phone);
+		stats.appendChild(url);
+		stats.appendChild(level);
+		//stats.appendChild(other);
+
+		//card.appendChild(h2);
+		card.appendChild(stats);
+
+		cards.appendChild(card);
+	});
+};
+
+getDirectory();
